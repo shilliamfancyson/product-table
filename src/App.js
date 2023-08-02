37597c1f5b374ca20c98923a9d87426509a9d59b
mike <youngmt16@gmail.com>
2023-08-02 17:39:06 -0400

    Adding new stuff

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ProductTable from "./ProductTable";
 function App() {
   return (
     <div>
-      <h1>Product Table</h1>
+      {/* <h1>Product Table</h1> */}
       <ProductTable />
     </div>
   );
--- src/ProductTable.js
+++ src/ProductTable.js
@@ -13,9 +13,10 @@ const ProductTable = () => {
     column: "Price",
     direction: "asc",
   });
+  const [hideNullLinks, setHideNullLinks] = useState(false);
+  const [hideMessaged, setHideMessaged] = useState(false);
 
   const totalProducts = products.length;
-
 
   useEffect(() => {
     firebase.initializeApp(firebaseConfig);
@@ -34,12 +35,25 @@ const ProductTable = () => {
           return a[column] < b[column] ? -direction : a[column] > b[column] ? direction : 0;
         });
 
-        setProducts(sortedProducts);
+        // Filter products based on the hideNullLinks and hideMessaged states
+        let filteredProducts = sortedProducts;
+        if (hideNullLinks) {
+          filteredProducts = sortedProducts.filter(
+            (product) => product.SoldByLink !== "null" && product.hasOwnProperty("SoldByLink")
+          );
+        }
+        if (hideMessaged) {
+          filteredProducts = filteredProducts.filter(
+            (product) => product.Messaged !== "Yes"
+          );
+        }
+
+        setProducts(filteredProducts);
       }
     });
 
     return () => productsRef.off("value");
-  }, [sortOrder]);
+  }, [sortOrder, hideNullLinks, hideMessaged]);
 
   const handleSort = (column) => {
     setSortOrder((prevSortOrder) => ({
@@ -50,77 +64,148 @@ const ProductTable = () => {
 
   const handleDeleteProduct = (asin) => {
     const productsRef = firebase.database().ref("products");
-
-    // Remove the product with the given ASIN from Firebase
     productsRef.child(asin).remove();
   };
 
-  // Function to format the timestamp into "last updated" string
   const formatTimestamp = (timestamp) => {
     const date = new Date(timestamp * 1000);
     return `Last Updated: ${date.toLocaleString()}`;
   };
 
-  // Get current products based on the current page
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
   const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
 
-  // Change page
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
-  return (
-    <div className="product-table-container">
-      <div className="total-products">Total Products: {totalProducts}</div>
+  const handleToggleMessaged = (asin, currentValue) => {
+    const productsRef = firebase.database().ref("products");
+    const newMessagedValue = currentValue === "Yes" ? "No" : "Yes";
+    productsRef.child(asin).update({ Messaged: newMessagedValue });
+  };
 
-      <table className="product-table">
-        <thead>
-          <tr>
-            <th onClick={() => handleSort("Title")}>Title</th>
-            <th onClick={() => handleSort("Price")}>Price</th>
-            <th onClick={() => handleSort("Reviews")}>Reviews</th>
-            <th onClick={() => handleSort("VideoCount")}>Video Count</th>
-            <th onClick={() => handleSort("timestamp")}>Last Updated</th>
-            <th>Link</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentProducts.map((product) => (
-            <tr key={product.ASIN}>
-              <td>{product.Title}</td>
-              <td>{product.Price}</td>
-              <td>{product.Reviews}</td>
-              <td>{product.VideoCount || "N/A"}</td>
-              <td>{formatTimestamp(product.timestamp)}</td>
-              <td>
-                <a href={product.Link} target="_blank" rel="noopener noreferrer">
-                  View on Amazon
-                </a>
-              </td>
-              <td>
-                <button onClick={() => handleDeleteProduct(product.ASIN)}>Delete</button>
-              </td>
+  const handleCopyTemplate = (template) => {
+    const el = document.createElement("textarea");
+    el.value = template;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    alert("Template copied to clipboard!");
+  };
+
+  return (
+    <div className="product-table-center-container">
+      <div className="product-table-container">
+        <div className="total-products">Total Products: {totalProducts}</div>
+
+        <div>
+          <input
+            type="checkbox"
+            checked={hideNullLinks}
+            onChange={() => setHideNullLinks((prev) => !prev)}
+          />
+          <label>Hide records with null Sold By Link</label>
+        </div>
+
+        <div>
+          <input
+            type="checkbox"
+            checked={hideMessaged}
+            onChange={() => setHideMessaged((prev) => !prev)}
+          />
+          <label>Hide Messaged Records</label>
+        </div>
+
+        <table className="product-table">
+          <thead>
+            <tr>
+              <th onClick={() => handleSort("Title")}>Title</th>
+              <th onClick={() => handleSort("Price")}>Price</th>
+              <th onClick={() => handleSort("Reviews")}>Reviews</th>
+              <th onClick={() => handleSort("VideoCount")}>Video Count</th>
+              <th onClick={() => handleSort("timestamp")}>Last Updated</th>
+              <th onClick={() => handleSort("Category")}>Category</th>
+              <th>Link</th>
+              <th>Delete</th>
+              <th onClick={() => handleSort("Messaged")}>Messaged</th>
+              <th>Template</th>
+              <th>Sold by link</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      {/* Pagination Controls */}
-      <div className="pagination">
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          Previous
-        </button>
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={indexOfLastProduct >= products.length}
-        >
-          Next
-        </button>
+          </thead>
+          <tbody>
+            {currentProducts.map((product) => (
+              <tr key={product.ASIN}>
+                <td>{product.Title}</td>
+                <td>{product.Price}</td>
+                <td>{product.Reviews}</td>
+                <td>{product.VideoCount || "N/A"}</td>
+                <td>{formatTimestamp(product.timestamp)}</td>
+                <td>{product.Category || "null"}</td>
+                <td>
+                  <a href={product.Link} target="_blank" rel="noopener noreferrer">
+                    View on Amazon
+                  </a>
+                </td>
+                <td>
+                  <button onClick={() => handleDeleteProduct(product.ASIN)}>Delete</button>
+                </td>
+                <td>
+                  {product.Messaged === null ? (
+                    <input
+                      type="checkbox"
+                      checked={false}
+                      onChange={() => handleToggleMessaged(product.ASIN, "No")}
+                    />
+                  ) : (
+                    <input
+                      type="checkbox"
+                      checked={product.Messaged === "Yes"}
+                      onChange={() =>
+                        handleToggleMessaged(product.ASIN, product.Messaged)
+                      }
+                    />
+                  )}
+                </td>
+                <td>
+                  {product.Template ? (
+                    <button onClick={() => handleCopyTemplate(product.Template)}>
+                      Copy
+                    </button>
+                  ) : (
+                    "null"
+                  )}
+                </td>
+                <td>
+                  {product.SoldByLink && product.SoldByLink !== "null" ? (
+                    <a href={product.SoldByLink} target="_blank" rel="noopener noreferrer">
+                      link
+                    </a>
+                  ) : (
+                    "null"
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div className="pagination">
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={indexOfLastProduct >= products.length}
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
